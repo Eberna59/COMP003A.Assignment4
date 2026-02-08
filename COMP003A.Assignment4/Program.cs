aca4e0d36004f354e9f0b8abcf0806bdc44ab6ed
evanbern59 <evansantos1202@gmail.com>
2026-02-07 21:36:51 -0800

    added saftey point and max stepps reached

--- COMP003A.Assignment4/Program.cs
+++ COMP003A.Assignment4/Program.cs
@@ -14,9 +14,31 @@
             while (menuActive)
             {
                 Console.WriteLine("1. Start/Continue Routine");
-                Console.WriteLine("2. Viw Progress");
+                Console.WriteLine("2. View Progress");
                 Console.WriteLine("3. Exit");
                 Console.Write("Enter Choice:");
+
+                int userChoice = int.Parse(Console.ReadLine());
+
+                if (userChoice == 1)
+                {
+                    if (progross >= safteyStopPoint)
+                    {
+                        Console.WriteLine("Saftey limit has reached. Routine Stopped.");
+                        continue;
+                    }
+
+                    if (progross >= maxAllowedSteps)
+                    {
+                        Console.WriteLine("Maximum steps reached. Routine is complete.");
+                        continue;
+                    }
+
+                    progross++;
+
+
+                }
+
             }
 
         }
