d667757aa7f12e5d62d5df40974a1e5a3a647add
evanbern59 <evansantos1202@gmail.com>
2026-02-07 21:18:38 -0800

    added the menu

--- COMP003A.Assignment4/Program.cs
+++ COMP003A.Assignment4/Program.cs
@@ -4,7 +4,21 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int progross = 0;
+            int maxAllowedSteps = 7;
+            int safteyStopPoint = 5;
+            int skippedStep = 3;
+
+            bool menuActive = true;
+
+            while (menuActive)
+            {
+                Console.WriteLine("1. Start/Continue Routine");
+                Console.WriteLine("2. Viw Progress");
+                Console.WriteLine("3. Exit");
+                Console.Write("Enter Choice:");
+            }
+
         }
     }
 }
